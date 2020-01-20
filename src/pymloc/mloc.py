b1bd5f5d69b0386e98cbbdc70847d1ce2dd10552
Daniel Bankmann <bankmann@math.tu-berlin.de>
2020-01-20 11:14:59 +0100

    Remove unused parts in mloc.py

--- src/pymloc/mloc.py
+++ src/pymloc/mloc.py
@@ -1,5 +1,3 @@
-from .solver.linear.quadratic import QuadraticLinearSolver
-from .solver.nonlinear.gauss_newton import GaussNewton
 import numpy as np
 
 """
@@ -10,12 +8,8 @@ We assume that the constraint function G_i are equal for all i.
 
 class MultiLevelOptimalControl(object):
     """Driver routine that initializes and handles all the solvers.
-
-    Currently assumes a buttom-to-top iterative structure.
     """
 
-    model2standard_solvers = {'QuadraticOptimizationProblem': QuadraticLinearSolver,
-                              'NonLinearLeastSquares': GaussNewton}
 
     def __init__(self, optimizations, solvers):
         self.optimizations = optimizations
@@ -27,27 +21,3 @@ class MultiLevelOptimalControl(object):
             self.is_bilevel = True
         else:
             self.is_bilevel = False
-
-
-
-    def _get_solver_models(self):
-        solvers_models = []
-        for i, ocp in enumerate(self.optimizations):
-            solvers_models.append({'model': ocp,
-                            'solver': self.model2standard_solvers[ocp.__class__.__name__],
-                            'level': i})
-        return solvers_models
-
-
-    def _initialize_solvers(self):
-        for solver_model in self.solver_models:
-            solver_model['solver_instance'] = solver_model['solver'](model=solver_model['model'])
-        # TODO ...
-        initial_state = np.array([[]])
-        return initial_state
-
-    def __call__(self):
-        state = self._initialize_solvers()
-        for i in range(4):
-            for solver in self.solver_instances:
-                state = solver.solve
