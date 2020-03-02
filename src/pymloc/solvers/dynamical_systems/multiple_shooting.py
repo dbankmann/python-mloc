Fix bug in multiple shooting. Do not transpose flows. Add tests for f_tilde computation and sensitivity computation. Add skeletons for f_tilde computation and sensitivity computation.

src/pymloc/solvers/dynamical_systems/multiple_shooting.py
@@ -39,8 +39,8 @@ class MultipleShooting(BaseSolver):
         flow_solver = solver_container_factory.get_solver_container(
             self._flow_problem).default_solver(self._flow_problem,
                                                time_interval, stepsize)
-        flow_solver.abs_tol = 1e-10
-        flow_solver.rel_tol = 1e-12
+        flow_solver.abs_tol = 1e-3
+        flow_solver.rel_tol = 1e-6
         return flow_solver.get_homogeneous_flows()
 
     def _get_shooting_values(self):
@@ -117,22 +117,29 @@ class MultipleShooting(BaseSolver):
 
     def _newton_step(self, current_node_states, rhs):
         shooting_matrix = self._get_shooting_matrix()
-        *shapefirst, shapem2, shapem1 = current_node_states.shape
-        next_nodes = current_node_states - np.linalg.solve(
-            shooting_matrix, rhs).reshape(
-                (*shapefirst, shapem1, shapem2), order='F').swapaxes(
-                    -2, -1)  #is this correct? Not for 2d!
-        import ipdb
-        ipdb.set_trace()
+        lin_sys_sol = np.linalg.solve(shooting_matrix, rhs)
+        next_nodes = current_node_states - self._restack(
+            lin_sys_sol, current_node_states.shape)
         return next_nodes
+
+    @staticmethod
+    def _unstack(array):
+        return np.moveaxis(array, -1, 0).reshape(-1, *array.shape[1:-1])
+
+    @staticmethod
+    def _restack(array, shape):
+        *shapefirst, shapem2, shapem1 = shape
+        restacked = np.einsum('ij...->j...i',
+                              array.reshape(shapem1, *shapefirst, shapem2))
+        return restacked
 
     def _get_newton_rhs(self, current_node_states):
         current_x_d = self._get_x_d(current_node_states)
         boundary_node_states = current_x_d[..., self._boundary_indices]
         bound_res = self._bvp.boundary_values.residual(boundary_node_states)
         diffs = current_node_states[..., 1:] - np.einsum(
-            'ijr,i...r->j...r', self._gis, current_node_states[..., :-1])
-        res_b = np.moveaxis(diffs, -1, 0).reshape(-1, *diffs.shape[1:-1])
+            'ijr,j...r->i...r', self._gis, current_node_states[..., :-1])
+        res_b = self._unstack(diffs)
         return np.concatenate((res_b, bound_res), axis=0)
 
     def _get_initial_guess(self, initial_guess=None):
@@ -152,17 +159,13 @@ class MultipleShooting(BaseSolver):
         projected_values = np.einsum('ijr,i...r->j...r', self._t2s,
                                      initial_guess)
 
-        import ipdb
-        ipdb.set_trace()
         for i in range(self.max_iter):
             residual = self._get_newton_rhs(projected_values)
-            if not self.abort(residual):
-                projected_values = self._newton_step(projected_values,
-                                                     residual)
+            if self.abort(residual):
+                break
+            projected_values = self._newton_step(projected_values, residual)
         x_d = self._get_x_d(projected_values)
         full_values = x_d + np.einsum('ijr,j...r->i...r', self._das, x_d)
-        import ipdb
-        ipdb.set_trace()
         return full_values
 
     def _get_x_d(self, projected_values):

src/pymloc/solvers/dynamical_systems/sensitivities.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 import scipy.linalg as linalg
 
 from pymloc.model.dynamical_system.flow_problem import LinearFlow
@@ -53,8 +54,17 @@ class SensitivitiesSolver(BaseSolver):
         #TODO: QR
         return np.solve(small_gammas @ small_gammas.T, small_gammas)
 
-    def _get_capital_f_tilde(self, localized_bvp):
-        solution = localized_bvp.solve()
+    def _get_capital_f_tilde(self, localized_bvp, solution):
+        a_dif = self._dynamical_system.a_theta
+        e_dif = self._dynamical_system.e_theta
+        f_dif = self._dynamical_system.f_theta
+        x_d = localized_bvp.dynamical_system.x_d(solution)
+        x_d_dot = np.einsum('ijk,jlk->ilk', localized_bvp.dynamical_system.d_d,
+                            x_d)
+
+        f_tilde = np.einsum('ijp,j->ip', a_dif, solution) - np.einsum(
+            'ijp,j->ip', e_dif, x_d_dot) + f_dif
+        return f_tilde
 
     def _setup_adjoint_sensitivity_bvp(self, localized_bvp, tau):
         n = self._dynamical_system.nn
@@ -88,8 +98,6 @@ class SensitivitiesSolver(BaseSolver):
              [(sel @ localized_bvp.dynamical_system.cal_projection(tau)).T]])
         z_gamma = localized_bvp.boundary_values.z_gamma
         z_gamma_new = linalg.block_diag(z_gamma, z_gamma)
-        import ipdb
-        ipdb.set_trace()
         boundary_values = MultipleBoundaryValues((bound_0, bound_tau, bound_f),
                                                  gamma, z_gamma_new)
         t1 = Time(t_0, tau)
@@ -117,11 +125,24 @@ class SensitivitiesSolver(BaseSolver):
         flow_problem = LinearFlow(time, adjoint_bvp.dynamical_system)
         return flow_problem
 
-    def run(self, parameters):
-        localized_bvp = self._bvp_param.get_sensitivity_bvp(parameters)
-        localized_bvp.solve()
+    def _compute_sensitivity(self, f_tilde, solution, adjoint_solution):
+        temp1 = np.einsum('ijk, j->ik', self._bvp_param.selector_theta,
+                          solution)
 
-        f_tilde = self._get_capital_f_tilde()
+        temp2 = self._bvp_param.selector @ localized_bvp.d_a(
+            tau) @ self._bvp_param.f_theta
+        temp3 = xi @ self._bvp_param.gamma_theta
+
+        temp4 = scipy.integrate(t0, tf, adjoint_solution.T @ f_tilde)
+
+        return temp1 - temp2 - temp3 - temp4
+
+    def run(self, parameters, tau):
+        localized_bvp = self._bvp_param.get_sensitivity_bvp(parameters)
+        solution = localized_bvp.solve()
+        f_tilde = self._get_capital_f_tilde(localized_bvp, solution)
+        adjoint_solution = self._get_adjoint_solution(localized_bvp, tau)
+        self._compute_sensitivity(f_tilde, adjoint_solution)
 
 
 solver_container_factory.register_solver(BVPSensitivities,

tests/test_solver/test_multiple_shooting.py
@@ -95,9 +95,24 @@ class TestMultipleShooting:
                          scipy_flow @ scipy_flow @ iv]).reshape(2,
                                                                 3,
                                                                 order='F')
-        import ipdb
-        ipdb.set_trace()
         assert np.allclose(erg, comp)
 
     def test_run_dae(self, ms_object_dae):
         erg = ms_object_dae.run()
+
+    def test_unstack(self, rand_shape):
+        a = rand_shape
+        b = MultipleShooting._unstack(a)
+        np.testing.assert_equal(a[..., 0], b[:a.shape[0], ...])
+
+    def test_restack(self, rand_shape):
+        b = MultipleShooting._unstack(rand_shape)
+        np.testing.assert_equal(MultipleShooting._restack(b, rand_shape.shape),
+                                rand_shape)
+
+    @pytest.fixture(params=np.arange(100))
+    def rand_shape(self):
+        dim = np.random.randint(2, 9)
+        shape = list(np.random.randint(1, 10, dim))
+        size = np.empty(shape).size
+        return np.arange(size).reshape(shape)