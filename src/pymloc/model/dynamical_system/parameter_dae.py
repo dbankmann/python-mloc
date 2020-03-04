Add TimeSolution object for storing solutions. Fix issues with sensitivity computation. ALMOST WORKING

src/pymloc/model/dynamical_system/parameter_dae.py
@@ -1,3 +1,5 @@
+import logging
+
 import jax
 import jax.numpy as np
 
@@ -5,17 +7,24 @@ from ..multilevel_object import MultiLevelObject
 from ..multilevel_object import local_object_factory
 from .representations import LinearFlowRepresentation
 
+logger = logging.getLogger(__name__)
+
 
 def jac_jax_reshaped(fun, shape, *args, **kwargs):
     def fun_raveled(*args_fun, **kwargs_fun):
         return fun(*args_fun, **kwargs_fun).ravel()
 
     def jac_reshaped(*points, **kw_points):
+        import ipdb
+        ipdb.set_trace()
         jac = jax.jacobian(fun_raveled, *args, **kwargs)
         jac_eval = np.atleast_2d(np.array(jac(*points, **kw_points)))
         diff_dim = jac_eval.shape[0]
-        return jac_eval.reshape(diff_dim,
-                                *shape).T  #Transpose for column major
+        logger.info("Reshaping jacobian with original shape: {}".format(
+            jac_eval.shape))
+        return np.einsum('i...->...i',
+                         jac_eval.reshape(diff_dim,
+                                          *shape))  #Transpose for column major
 
     return jac_reshaped
 

src/pymloc/model/sensitivities/boundary_dae.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from ..dynamical_system.parameter_bvp import ParameterBoundaryValueProblem
+from ..dynamical_system.parameter_dae import jac_jax_reshaped
 from ..solvable import Solvable
 
 
@@ -20,7 +21,9 @@ class BVPSensitivities(Solvable):
         self._time_interval = self._bvp.time_interval
         self._n_param = n_param
         if selector is None:
-            selector = np.identity(self._dynamical_system.nn)
+            selector = lambda p: np.identity(self._dynamical_system.nn)
+            self._sel_shape = (self._dynamical_system.nn,
+                               self._dynamical_system.nn)
         self._selector = selector
 
     @property
@@ -38,6 +41,15 @@ class BVPSensitivities(Solvable):
     @property
     def selector(self):
         return self._selector
+
+    @property
+    def selector_theta(self):
+        shape = self._sel_shape
+        return jac_jax_reshaped(self.selector, shape)
+
+    @property
+    def time_interval(self):
+        return self._time_interval
 
     def get_sensitivity_bvp(self, parameters):
         return self._bvp.get_localized_object(parameters=parameters)