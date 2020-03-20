Add test for optimal control and compare with analytic solution. Fix bug in multiple shooting with wrong sign in projection formula. Fix bug in _get_intermediate_values, when an inner shooting node is not included in the time_grid.

src/pymloc/solvers/dynamical_systems/multiple_shooting.py
@@ -9,6 +9,7 @@ from ...model.dynamical_system.boundary_value_problem import MultipleBoundaryVal
 from ...model.dynamical_system.flow_problem import LinearFlow
 from ...model.dynamical_system.initial_value_problem import InitialValueProblem
 from ...model.optimization.optimal_control import LQOptimalControl
+from ...model.variables.time_function import Time
 from ...solver_container import solver_container_factory
 from ..base_solver import BaseSolver
 from ..base_solver import TimeSolution
@@ -201,7 +202,7 @@ class MultipleShooting(BaseSolver):
                 break
             projected_values = self._newton_step(projected_values, residual)
         x_d = self._get_x_d(projected_values)
-        full_node_values = x_d + np.einsum('ijr,j...r->i...r', self._das, x_d)
+        full_node_values = x_d - np.einsum('ijr,j...r->i...r', self._das, x_d)
         node_solution = TimeSolution(self._shooting_nodes, full_node_values)
         time_grid_solution = self._get_intermediate_values(node_solution)
         return time_grid_solution, node_solution
@@ -226,12 +227,19 @@ class MultipleShooting(BaseSolver):
         for i, node in enumerate(node_solution.time_grid):
             loweridx = idx[i]
             upperidx = idx[i + 1]
-            interval = self._solution_time_grid[loweridx:upperidx]
-            x0 = node_solution(node)
+            grid = self._solution_time_grid[loweridx:upperidx]
             t0 = node
+            if t0 in grid:
+                idx_increment = 0
+            else:
+                idx_increment = 1
+
+            x0 = node_solution(node)
+            tf = grid[-1]
+            interval = Time(t0, tf, grid)
             x0_times = self._ivp_problem.solve(interval, x0)
             solution[..., loweridx:upperidx] = np.atleast_2d(
-                x0_times.solution.T).T
+                x0_times.solution.T).T[:, idx_increment:]
         return TimeSolution(self._solution_time_grid, solution)
 
     def _get_x_d(self, projected_values):

tests/test_model/test_dynamical_system/test_representation.py
@@ -1,7 +1,100 @@
+import numpy as np
 import pytest
+
+from pymloc.model.dynamical_system.representations import LinearFlowRepresentation
+from pymloc.model.variables.container import StateVariablesContainer
 
 from .test_dae import TestLinearDAE
 
 
+@pytest.fixture
+def dae():
+    def e(t):
+        return np.array([[0., 1., 0.], [-1., 0., 0.], [-0., 0., 0.]])
+
+    def a(t):
+        return np.array([[0., -1., 1.], [-1., 3., 0.], [1., 0., 1.]])
+
+    def f(t):
+        return np.array([0., 0., 0.])
+
+    variables = StateVariablesContainer(3)
+    flow = LinearFlowRepresentation(variables, e, a, f, 3)
+    flow.init_rank()
+    return flow
+
+
 class TestFlowRepresentation(TestLinearDAE):
-    pass
+    def test_projection_ehat(self, dae):
+        proj_val = dae.ehat_1(0.)
+
+        ref_val = np.array([[0., 1., 0.], [-1., 0., 0.]])
+        assert np.allclose(proj_val, ref_val)
+
+    def test_projection_ehat2(self, dae):
+        proj_val = dae.ehat_2(0.)
+
+        ref_val = np.zeros(3)
+        assert np.allclose(proj_val, ref_val)
+
+    def test_projection_ahat(self, dae):
+        proj_val = dae.ahat_1(0.)
+
+        ref_val = np.array([[0., -1., 1.], [-1., 3., 0.]])
+        assert np.allclose(proj_val, ref_val)
+
+    def test_projection_ahat2(self, dae):
+        proj_val = dae.ahat_2(0.)
+        ref_val = np.array([[1., 0., 1.]])
+        assert np.allclose(proj_val, ref_val)
+
+    def test_projection_fhat(self, dae):
+        proj_val = dae.fhat_1(0.)
+
+        ref_val = np.zeros(2)
+        assert np.allclose(proj_val, ref_val)
+
+    def test_projection_fhat2(self, dae):
+        proj_val = dae.fhat_2(0.)
+        ref_val = np.zeros(1)
+        assert np.allclose(proj_val, ref_val)
+
+    def test_projection(self, dae):
+        proj_val = dae.projection(0.)
+
+        ref_val = np.array([[1., 0., 0.], [0., 1., 0.], [0., 0., 0.]])
+
+        assert np.allclose(proj_val, ref_val)
+
+    def test_projection(self, dae):
+        proj_val = dae.projection_complement(0.)
+
+        ref_val = np.array([[0., 0., 0.], [0., 0., 0.], [0., 0., 1.]])
+
+        assert np.allclose(proj_val, ref_val)
+
+    def test_projection_der(self, dae):
+        proj_val = dae.projection_derivative(0.)
+
+        ref_val = np.zeros((3, 3))
+
+        assert np.allclose(proj_val, ref_val)
+
+    def test_projection_da(self, dae):
+        proj_val = dae.d_a(0.)
+
+        ref_val = np.array([[0., 0., 0.], [0., 0., 0.], [1., 0., 1.]])
+        assert np.allclose(proj_val, ref_val)
+
+    def test_projection_cal(self, dae):
+        proj_val = dae.cal_projection(0.)
+
+        ref_val = np.array([[1., 0., 0.], [0., 1., 0.], [-1., 0., 0.]])
+        assert np.allclose(proj_val, ref_val)
+
+    def test_projection_dd(self, dae):
+        proj_val = dae.d_d(0.)
+
+        ref_val = np.array([[1., -3., 0.], [-1., -1., 0.], [0., 0., 0.]])
+
+        assert np.allclose(proj_val, ref_val)

tests/test_model/test_optimization/test_optimal_control.py
@@ -43,7 +43,7 @@ def lqr_obj_args():
     s = lambda t: np.array([[0.]])
     r = lambda t: np.array([[1.]])
     m = np.array([[0.]])
-    time = Time(0., 10.)
+    time = Time(0., 2.)
     return time, q, s, r, m
 
 
@@ -63,5 +63,34 @@ class TestLQOptimalControl(TestLocalOptimizationObject):
         return super().test_solve(loc_opt, stepsize=1e-3)
 
     def test_solve2(self, loc_opt):
-        time = Time(0., 10.)
-        return super().test_solve2(loc_opt, time, stepsize=1e-3)
+        time = Time(0., 2.)
+        sol = super().test_solve2(loc_opt, time, stepsize=1e-3)
+        ref_sol = None
+
+    def test_solve3(self, loc_opt):
+        loc_opt.init_solver(stepsize=1e-3, abs_tol=1e-12, rel_tol=1e-12)
+        theta = 2
+        time = Time(0., 2.)
+        time.grid = np.linspace(0., 2., 100)
+        sol = loc_opt.solve(time, flow_abs_tol=1e-12, flow_rel_tol=1e-12)[0]
+        x0 = loc_opt.constraint.initial_value
+        for t in sol.time_grid:
+            refsol = self.refsol(theta, time, x0, t)
+            assert np.allclose(sol(t), refsol)
+
+    def refsol(self, theta, time, x0, t):
+        tf = time.t_f
+        t0 = time.t_0
+        exp0 = np.exp(2 * theta * (tf - t0))
+        exp1 = np.exp(-(t + t0) * theta)
+        exp2 = np.exp(2 * t * theta)
+        exp3 = np.exp(2 * tf * theta)
+        tmp1 = theta + exp0 * (theta + 1) - 1
+        tmp2 = np.array([
+            -(exp2 - exp3) * (theta**2 - 1),
+            (exp2 * (theta - 1) + exp3 * (theta + 1)),
+            (exp2 - exp3) * (theta**2 - 1)
+        ])
+
+        refsol = tmp1**-1 * tmp2 * exp1 * x0
+        return refsol