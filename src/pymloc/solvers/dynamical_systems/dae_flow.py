Split sensitivities solver in Forward and Adjoint objects. Fix several bugs for correct computation of Adjoint and Forward sensisitivities. Add forward solve in rhs of MultipleShooting which includes right-hand-side. Fix problems with node detection. Add meaningful tests with known correct results for both Adjoint and Forward sensitivities. Add parameter optimal control problem test.

src/pymloc/solvers/dynamical_systems/dae_flow.py
@@ -26,6 +26,11 @@ class ProjectionDAEFlowIntegrator(DAEFlow):
         self._nn = dae_flow_instance._nn
 
     @property
+    def _intervals(self):
+        time_grid = self._time_interval.grid
+        return zip(time_grid, time_grid[1:])
+
+    @property
     def stepsize(self):
         return self._stepsize
 
@@ -59,7 +64,7 @@ class ProjectionDAEFlowIntegrator(DAEFlow):
     def _save_homogeneous_flows(self):
         n = self._nn
         time_grid = self.time_interval.grid
-        intervals = zip(time_grid, time_grid[1:])
+        intervals = self._intervals
         nflows = len(time_grid) - 1
         flows = np.zeros((n, n, nflows), order='F')
         #TODO: Paralellize
@@ -81,8 +86,7 @@ class ProjectionDAEFlowIntegrator(DAEFlow):
             return self.model.flow_dae.d_d(t)
 
         h = self.stepsize
-        integrator = ode(f, jac).set_integrator('vode',
-                                                method='bdf',
+        integrator = ode(f, jac).set_integrator('dopri5',
                                                 atol=self.abs_tol,
                                                 rtol=self.rel_tol)
         for i, unit_vector in enumerate(np.identity(n)):
@@ -90,6 +94,40 @@ class ProjectionDAEFlowIntegrator(DAEFlow):
             integrator.set_initial_value(unit_vector, t0)
             flow[:, i] = integrator.integrate(tf)
         return flow
+
+    def forward_solve_differential(self, node_values):
+        end_values = np.zeros(node_values.shape)
+        for i, ((tm, tp),
+                values) in enumerate(zip(self._intervals, node_values.T)):
+            logger.info(
+                "Computing inhomogeneous solution in the interval ({}, {})".
+                format(tm, tp))
+            end_values[..., i] = self._forward_solve(tm, tp, values)
+
+        return end_values
+
+    def _forward_solve(self, tm, tp, values):
+        shape = values.shape
+        values2d = np.atleast_2d(values)
+        value_arr = np.empty(values2d.shape).T
+
+        def jac(t, x):
+            return self.model.flow_dae.d_d(t)
+
+        for i, value in enumerate(values2d):
+
+            def f(t, x):
+                return self.model.flow_dae.d_d(t) @ x + np.atleast_2d(
+                    self.model.flow_dae.f_d(t).T).T[:, i]
+
+            integrator = ode(f, jac).set_integrator('dopri5',
+                                                    atol=self.abs_tol,
+                                                    rtol=self.rel_tol)
+            integrator.set_initial_value(value, tm)
+            value_arr[:, i] = integrator.integrate(tp)
+        if len(shape) == 1:
+            value_arr = np.squeeze(value_arr, axis=1)
+        return value_arr
 
 
 solver_container_factory.register_solver(LinearFlow,

src/pymloc/solvers/dynamical_systems/multiple_shooting.py
@@ -174,10 +174,17 @@ class MultipleShooting(BaseSolver):
         current_x_d = self._get_x_d(current_node_states)
         boundary_node_states = current_x_d[..., self._boundary_indices]
         bound_res = self._bvp.boundary_values.residual(boundary_node_states)
-        diffs = current_node_states[..., 1:] - np.einsum(
-            'ijr,j...r->i...r', self._gis, current_node_states[..., :-1])
+        computed_node_states = self._forward_projected_nodes(
+            current_node_states)
+        diffs = current_node_states[..., 1:] - computed_node_states[..., :-1]
         res_b = self._unstack(diffs)
         return np.concatenate((res_b, bound_res), axis=0)
+
+    def _forward_projected_nodes(self, node_values):
+        solver = self._flow_solver
+        lift_up = self._get_x_d(node_values)
+        sol = solver.forward_solve_differential(lift_up)
+        return self._project_values(sol)
 
     def _get_initial_guess(self, initial_guess=None):
         shape = self._dynamical_system.variables.shape + (
@@ -190,19 +197,21 @@ class MultipleShooting(BaseSolver):
                     initial_guess.shape, shape))
         return initial_guess
 
+    def _project_values(self, values):
+        return np.einsum('ijr,i...r->j...r', self._t2s, values)
+
     def _run(self, time_interval, initial_guess=None, *args, **kwargs):
         self._init_solver(time_interval, *args, **kwargs)
         initial_guess = self._get_initial_guess(initial_guess)
-        projected_values = np.einsum('ijr,i...r->j...r', self._t2s,
-                                     initial_guess)
-
+        projected_values = self._project_values(initial_guess)
         for i in range(self.max_iter):
             residual = self._get_newton_rhs(projected_values)
             if self.abort(residual):
                 break
             projected_values = self._newton_step(projected_values, residual)
         x_d = self._get_x_d(projected_values)
-        full_node_values = x_d - np.einsum('ijr,j...r->i...r', self._das, x_d)
+        full_node_values = x_d - np.einsum('ijr,j...r->i...r', self._das,
+                                           x_d) - self._fas
         node_solution = TimeSolution(self._shooting_nodes, full_node_values)
         time_grid_solution = self._get_intermediate_values(node_solution)
         return time_grid_solution, node_solution
@@ -229,7 +238,7 @@ class MultipleShooting(BaseSolver):
             upperidx = idx[i + 1]
             grid = self._solution_time_grid[loweridx:upperidx]
             t0 = node
-            if t0 in grid:
+            if np.allclose(t0, grid[0], 1e-16):
                 idx_increment = 0
             else:
                 idx_increment = 1
@@ -247,9 +256,15 @@ class MultipleShooting(BaseSolver):
 
     def _set_d_as(self):
         das = np.zeros((self._nn, self._nn, self._n_shooting_nodes), order='F')
+        shape = self._dynamical_system.variables.shape
+        fas = np.zeros((*shape, self._n_shooting_nodes), order='F')
+
+        self._dynamical_system.init_rank()
         for i, node in enumerate(self._shooting_nodes):
             das[:, :, i] = self._dynamical_system.d_a(node)
+            fas[..., i] = self._dynamical_system.f_a(node)
         self._das = das
+        self._fas = fas
 
 
 solver_container_factory.register_solver(MultipleBoundaryValueProblem,

tests/conftest.py
@@ -25,7 +25,7 @@ from pymloc.model.variables.container import InputOutputStateVariables
 from pymloc.model.variables.container import StateVariablesContainer
 from pymloc.model.variables.time_function import StateVariables
 from pymloc.model.variables.time_function import Time
-from pymloc.solvers.dynamical_systems.sensitivities import SensitivitiesSolver
+from pymloc.solvers.dynamical_systems.adjoint_sensitivities import AdjointSensitivitiesSolver
 
 np.set_printoptions(precision=4)
 
@@ -62,7 +62,9 @@ def bvp_sens_object(linear_param_bvp):
 
 @pytest.fixture
 def sens_solver(bvp_sens_object):
-    return SensitivitiesSolver(bvp_sens_object, rel_tol=1e-1, abs_tol=1e-1)
+    return AdjointSensitivitiesSolver(bvp_sens_object,
+                                      rel_tol=1e-1,
+                                      abs_tol=1e-1)
 
 
 @pytest.fixture(params=np.arange(0.1, 2.5, 1.1))

tests/test_model/test_optimization/test_optimal_control.py
@@ -11,6 +11,35 @@ from pymloc.model.variables.time_function import Time
 from .test_local_optimization import TestLocalOptimizationObject
 
 
+def compute_ref_sol(theta, time, x0, t):
+    tf = time.t_f
+    t0 = time.t_0
+    exp0 = np.exp(2 * theta * (tf - t0))
+    exp1 = np.exp(-(t + t0) * theta)
+    exp2 = np.exp(2 * t * theta)
+    exp3 = np.exp(2 * tf * theta)
+    tmp1 = theta + exp0 * (theta + 1) - 1
+    tmp2 = np.array([
+        -(exp2 - exp3) * (theta**2 - 1),
+        (exp2 * (theta - 1) + exp3 * (theta + 1)),
+        (exp2 - exp3) * (theta**2 - 1)
+    ])
+
+    refsol = tmp1**-1 * tmp2 * exp1 * x0
+    return refsol
+
+
+def compare_sol_ref_sol(loc_opt, theta=2.):
+    loc_opt.init_solver(stepsize=1e-3, abs_tol=1e-12, rel_tol=1e-12)
+    time = Time(0., 2.)
+    time.grid = np.linspace(0., 2., 100)
+    sol = loc_opt.solve(time, flow_abs_tol=1e-12, flow_rel_tol=1e-12)[0]
+    x0 = loc_opt.constraint.initial_value
+    for t in sol.time_grid:
+        refsol = compute_ref_sol(theta, time, x0, t)
+        assert np.allclose(sol(t), refsol)
+
+
 @pytest.fixture
 def dae_control():
     variables = InputStateVariables(1, 1, time=Time(0., 2.))
@@ -68,29 +97,4 @@ class TestLQOptimalControl(TestLocalOptimizationObject):
         ref_sol = None
 
     def test_solve3(self, loc_opt):
-        loc_opt.init_solver(stepsize=1e-3, abs_tol=1e-12, rel_tol=1e-12)
-        theta = 2
-        time = Time(0., 2.)
-        time.grid = np.linspace(0., 2., 100)
-        sol = loc_opt.solve(time, flow_abs_tol=1e-12, flow_rel_tol=1e-12)[0]
-        x0 = loc_opt.constraint.initial_value
-        for t in sol.time_grid:
-            refsol = self.refsol(theta, time, x0, t)
-            assert np.allclose(sol(t), refsol)
-
-    def refsol(self, theta, time, x0, t):
-        tf = time.t_f
-        t0 = time.t_0
-        exp0 = np.exp(2 * theta * (tf - t0))
-        exp1 = np.exp(-(t + t0) * theta)
-        exp2 = np.exp(2 * t * theta)
-        exp3 = np.exp(2 * tf * theta)
-        tmp1 = theta + exp0 * (theta + 1) - 1
-        tmp2 = np.array([
-            -(exp2 - exp3) * (theta**2 - 1),
-            (exp2 * (theta - 1) + exp3 * (theta + 1)),
-            (exp2 - exp3) * (theta**2 - 1)
-        ])
-
-        refsol = tmp1**-1 * tmp2 * exp1 * x0
-        return refsol
+        compare_sol_ref_sol(loc_opt)

tests/test_model/test_parameter_dependent_optimal_control.py
@@ -1,8 +1,10 @@
+import jax.numpy as jnp
 import numpy as np
 import pytest
 
 from pymloc.model.control_system.parameter_dae import LinearParameterControlSystem
 from pymloc.model.domains import RNDomain
+from pymloc.model.dynamical_system.parameter_dae import jac_jax_reshaped
 from pymloc.model.optimization.parameter_optimal_control import ParameterDependentOptimalControl
 from pymloc.model.optimization.parameter_optimal_control import ParameterLQRConstraint
 from pymloc.model.optimization.parameter_optimal_control import ParameterLQRObjective
@@ -11,10 +13,13 @@ from pymloc.model.variables import NullVariables
 from pymloc.model.variables import ParameterContainer
 from pymloc.model.variables.time_function import Time
 
+from .test_optimization.test_optimal_control import compare_sol_ref_sol
+from .test_optimization.test_optimal_control import compute_ref_sol
+
 
 @pytest.fixture
 def variables():
-    loc_vars = InputStateVariables(1, 1)
+    loc_vars = InputStateVariables(1, 1, time=Time(0., 2.))
     hl_vars = ParameterContainer(1, domain=RNDomain(1))
     return hl_vars, loc_vars
 
@@ -48,7 +53,7 @@ def param_control(variables):
 @pytest.fixture
 def initial_value():
     def ivp(p):
-        return np.array([[p]])
+        return np.array([[2.]])
 
     return ivp
 
@@ -56,7 +61,7 @@ def initial_value():
 @pytest.fixture
 def q():
     def q(p, t):
-        return np.array([[p, 0.], [0., 1.]])
+        return jnp.array([[p**2. - 1.]])
 
     return q
 
@@ -64,7 +69,7 @@ def q():
 @pytest.fixture
 def s():
     def s(p, t):
-        return np.zeros((2, 1))
+        return np.zeros((1, 1))
 
     return s
 
@@ -82,10 +87,12 @@ def m():
     def m(p):
         return np.array([[0.]])
 
+    return m
+
 
 @pytest.fixture
 def pdoc_objective(q, s, r, m, variables):
-    time = Time(0., 10.)
+    time = Time(0., 2.)
     return ParameterLQRObjective(*variables, time, q, s, r, m)
 
 
@@ -101,10 +108,74 @@ def pdoc_object(variables, pdoc_objective, pdoc_constraint):
 
 
 class TestPDOCObject:
+    def refsol(self, theta, t0, tf, t, x01):
+        refsol = np.array(
+            [[(1 / ((-1 + theta + np.exp(2 * (-t0 + tf) * theta) *
+                     (1 + theta))**2)) * np.exp(-(t + t0) * theta) * x01 *
+              (np.exp(-2 * (t0 - 2 * tf) * theta) * (1 + theta)**2 *
+               (1 + t + t0 *
+                (-1 + theta) - t * theta) - np.exp(2 * (t - t0 + tf) * theta) *
+               (1 + theta)**2 *
+               (1 + 2 * tf + t * (-1 + theta) + t0 *
+                (-1 + theta) - 2 * tf * theta) - np.exp(2 * t * theta) * 2 *
+               (-1 + theta)**2 * (1 + t * (1 + theta) - t0 *
+                                  (1 + theta)) + np.exp(2 * tf * theta) *
+               (-1 + theta)**2 * (1 - t * (1 + theta) - t0 *
+                                  (1 + theta) + 2 * tf * (1 + theta)))],
+             [(1 / ((-1 + theta + np.exp(2 * (-t0 + tf) * theta) *
+                     (1 + theta))**2)) * np.exp(-(t + t0) * theta) * x01 *
+              (np.exp(2 * t * theta) * (t - t0) *
+               (-1 + theta)**2 + np.exp(-2 * t0 * theta + 4 * tf * theta) *
+               (-t + t0) * (1 + theta)**2 + np.exp(2 * tf * theta) *
+               (-2 + t + t0 - 2 * tf -
+                (t + t0 - 2 * tf) * theta**2) + np.exp(2 *
+                                                       (t - t0 + tf) * theta) *
+               (2 - t - t0 + 2 * tf + (t + t0 - 2 * tf) * theta**2))]])
+        return refsol
+
     def test_eval_weights(self, pdoc_object):
         random_p, = pdoc_object.parameters.get_random_values()
         random_x, random_u, random_t = pdoc_object.state_input.get_random_values(
         )
         local_lq = pdoc_object.get_localized_object(hl_value=random_p)
         weights = local_lq.objective.integral_weights(1.)
-        assert weights.shape == (3, 3) and weights[0, 0] == random_p[0]
+        final_weight = local_lq.objective.final_weight
+        assert final_weight.shape == (1, 1)
+        assert weights.shape == (2, 2) and weights[0, 0] == random_p[0]**2 - 1
+
+    def test_solution(self, pdoc_object):
+        random_p, = pdoc_object.parameters.get_random_values()
+        local_lq = pdoc_object.get_localized_object(hl_value=random_p)
+        compare_sol_ref_sol(local_lq, random_p[0])
+
+    def test_q_theta(self, pdoc_object):
+        parameters = np.array([2.])
+        q = pdoc_object.objective.q
+        jac = jac_jax_reshaped(q, (1, 1))
+        jac_val = jac(parameters, 2.)
+
+        assert jac_val == np.array([[2 * parameters[0]]])
+
+    def test_int_theta(self, pdoc_object):
+        parameters = np.array([2.])
+        intw = pdoc_object.objective.integral_weights
+        jac = jac_jax_reshaped(intw, (2, 2))
+        jac_val = jac(parameters, 2.)
+
+    def test_sensitivities(self, pdoc_object):
+        sens = pdoc_object.get_sensitivities()
+        sens.init_solver(abs_tol=1e-8, rel_tol=1e-8)
+        sol = sens.solve(parameters=np.array([2.]), tau=1.)
+        rsol = self.refsol(2., 0., 2., 1., 2.)
+        ref = np.block([[rsol], [-rsol[0]]])
+        assert np.allclose(ref, sol, rtol=1e-9, atol=1e-1)
+
+    def test_forward_sensitivities(self, pdoc_object):
+        sens = pdoc_object.get_sensitivities()
+        solver = sens._available_solvers.solvers[1]
+        sens.solver = solver
+        sens.init_solver(abs_tol=1e-8, rel_tol=1e-8)
+        sol = sens.solve(parameters=np.array([2.]))
+        rsol = self.refsol(2., 0., 2., 1., 2.)
+        ref = np.block([[rsol], [-rsol[0]]])
+        assert np.allclose(ref, sol[0](1.), atol=1e-2, rtol=1e-9)

tests/test_solver/test_sensitivities.py
@@ -1,12 +1,111 @@
 from copy import deepcopy
 
+import jax.numpy as jnp
 import numpy as np
 import pytest
 
+from pymloc.model.domains import RNDomain
 from pymloc.model.dynamical_system.boundary_value_problem import MultipleBoundaryValueProblem
 from pymloc.model.dynamical_system.flow_problem import LinearFlow
 from pymloc.model.dynamical_system.initial_value_problem import InitialValueProblem
+from pymloc.model.dynamical_system.parameter_bvp import ParameterBoundaryValueProblem
+from pymloc.model.dynamical_system.parameter_bvp import ParameterBoundaryValues
+from pymloc.model.dynamical_system.parameter_dae import LinearParameterDAE
+from pymloc.model.sensitivities.boundary_dae import BVPSensitivities
+from pymloc.model.variables import NullVariables
+from pymloc.model.variables import ParameterContainer
+from pymloc.model.variables import StateVariablesContainer
 from pymloc.model.variables.time_function import Time
+from pymloc.solvers.dynamical_systems.adjoint_sensitivities import AdjointSensitivitiesSolver
+
+
+@pytest.fixture
+def bvp_sens_object(linear_param_bvp):
+    return BVPSensitivities(linear_param_bvp, n_param=1)
+
+
+@pytest.fixture
+def sens_solver_sine(bvp_sens_object):
+    return AdjointSensitivitiesSolver(bvp_sens_object,
+                                      rel_tol=1e-1,
+                                      abs_tol=1e-1)
+
+
+@pytest.fixture
+def sens_solver_sine2(bvp_sens_object):
+    bvpsens2 = deepcopy(bvp_sens_object)
+    bvpsens2.dynamical_system._a = lambda p, t: jnp.array([[0., 1.],
+                                                           [-p**2, 0.]])
+    bvpsens2.boundary_value_problem.boundary_values.inhomogeneity = lambda p: jnp.array(
+        [0., 2.])
+
+    return AdjointSensitivitiesSolver(bvpsens2, rel_tol=1e-6, abs_tol=1e-6)
+
+
+@pytest.fixture
+def sens_solver_sine3(bvp_sens_object):
+    bvpsens2 = deepcopy(bvp_sens_object)
+    bound_f = lambda p: jnp.array([[0., 0.], [p, 0.]])
+    bvpsens2.boundary_value_problem.boundary_values.boundary_f = bound_f
+    bvs = bvpsens2.boundary_value_problem.boundary_values.boundary_values
+    bvs = list(bvs)
+    bvs[1] = bound_f
+    bvpsens2.boundary_value_problem.boundary_values._boundary_values = bvs
+
+    bvpsens2.boundary_value_problem.boundary_values.inhomogeneity = lambda p: jnp.array(
+        [0., 2.])
+    return AdjointSensitivitiesSolver(bvpsens2, rel_tol=1e-6, abs_tol=1e-6)
+
+
+@pytest.fixture
+def linear_param_bvp(linear_param_dae, param_vars):
+    initial_value = lambda p: jnp.array([[1., 0.], [0., 0.]])
+    final_value = lambda p: jnp.array([[0., 0.], [1., 0.]])
+    inhomogeneity = lambda p: jnp.array([0., p])
+    t = Time(0., np.pi / 2., time_grid=np.linspace(0., np.pi / 2., 10))
+    bvs = ParameterBoundaryValues(*param_vars, initial_value, final_value,
+                                  inhomogeneity, 2)
+    return ParameterBoundaryValueProblem(*param_vars, t, linear_param_dae, bvs)
+
+
+@pytest.fixture
+def linear_param_dae(param_vars, e_lin_param_dae, a_lin_param_dae,
+                     f_lin_param_dae):
+    return LinearParameterDAE(*param_vars, e_lin_param_dae, a_lin_param_dae,
+                              f_lin_param_dae, 2)
+
+
+@pytest.fixture
+def param_vars():
+    states = StateVariablesContainer(2)
+    domain = RNDomain(1)
+    parameters = ParameterContainer(1, domain)
+    null_vars = NullVariables()
+    return null_vars, parameters, states
+
+
+@pytest.fixture
+def e_lin_param_dae():
+    def e(p, t):
+        return np.identity(2)
+
+    return e
+
+
+@pytest.fixture
+def a_lin_param_dae():
+    def a(p, t):
+        return jnp.array([[0., 1.], [-1., 0.]])
+
+    return a
+
+
+@pytest.fixture
+def f_lin_param_dae():
+    def f(p, t):
+        return np.array([0., 0.])
+
+    return f
 
 
 class TestBVPSensitivitiesSolver:
@@ -21,8 +120,9 @@ class TestBVPSensitivitiesSolver:
         parameters = localized_bvp._localization_parameters[0]
         time = deepcopy(localized_bvp.time_interval)
         stepsize = 0.5e-0
-        time.grid = np.hstack((np.arange(time.t_0, tau + stepsize, stepsize),
-                               np.arange(tau, time.t_f + stepsize, stepsize)))
+        time.grid = np.hstack(
+            (np.arange(time.t_0, tau,
+                       stepsize), np.arange(tau, time.t_f, stepsize)))
         sol = sens_solver._get_adjoint_solution(localized_bvp, parameters, tau,
                                                 time)
 
@@ -43,3 +143,26 @@ class TestBVPSensitivitiesSolver:
     @pytest.mark.parametrize("tau", np.arange(0.1, 1., 0.4))
     def test_run(self, sens_solver, localized_bvp, tau):
         sens_solver.run(localized_bvp._localization_parameters[0], tau)
+
+    def test_sens_sine(self, sens_solver_sine):
+        sol = sens_solver_sine.run(np.array(2.), 1.)
+        assert np.allclose(sol.T,
+                           np.array([np.sin(1.), np.cos(1.)]),
+                           atol=1e-9,
+                           rtol=1e-4)
+
+    def test_sens_sine2(self, sens_solver_sine2):
+        sol = sens_solver_sine2.run(np.array(1.), 1.)
+        assert np.allclose(
+            sol.T,
+            np.array([2 * np.cos(1.), 2 * np.cos(1.) - 2 * np.sin(1.)]),
+            atol=1e-0,
+            rtol=1e-9)
+
+    def test_sens_sine3(self, sens_solver_sine3):
+        sol = sens_solver_sine3.run(np.array(2.), 1.)
+        assert np.allclose(sol.T,
+                           -2 * .25 *
+                           np.array([np.sin(1.), np.cos(1.)]),
+                           atol=1e-8,
+                           rtol=1e-7)