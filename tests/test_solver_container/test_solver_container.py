b3b0be5dd3aee129341e301932d1e3247d07c262
Daniel Bankmann <bankmann@math.tu-berlin.de>
2020-03-30 11:08:30 +0200

    Fix failing test for solver_container test.

--- tests/test_solver_container/test_solver_container.py
+++ tests/test_solver_container/test_solver_container.py
@@ -1,11 +1,13 @@
+import pytest
+
 from pymloc.solver_container import solver_container_factory
 from pymloc.solvers import NullSolver
-import pytest
 
 
 class TestSolverContainer:
     def test_set_default_solver(self, local_opt):
         solver_container = solver_container_factory.get_solver_container(
             local_opt[0])
-        solver_container.default_solver(NullSolver)
-        assert solver_container.default_solver == NullSolver
+        solver_container.default_solver = solver_container._solver_tuple_obj(
+            NullSolver, None)
+        assert solver_container.default_solver.solver == NullSolver
