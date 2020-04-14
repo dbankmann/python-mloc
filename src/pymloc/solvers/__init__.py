aac34cbe66aa63537c6534e47aa037ae6ecf4513
Daniel Bankmann <bankmann@math.tu-berlin.de>
2020-04-14 12:06:57 +0200

    Add mloc to solvers import.

--- src/pymloc/solvers/__init__.py
+++ src/pymloc/solvers/__init__.py
@@ -1,6 +1,6 @@
 from .base_solver import BaseSolver
 from .null_solver import NullSolver
-
-from . import nonlinear
-from . import linear
 from . import dynamical_systems
+from . import linear
+from . import mloc
+from . import nonlinear
