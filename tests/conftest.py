781274951c73f41a5f779992002cadda02d0a030
Daniel Bankmann <bankmann@math.tu-berlin.de>
2020-04-14 12:39:20 +0200

    Merge global fixtures into conftest.py.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import jax.numpy as jnp
 import numpy as np
 import pytest
 
-from pymloc.model.control_system.dae import LinearControlSystem
+from pymloc.model.control_system.parameter_dae import LinearParameterControlSystem
 from pymloc.model.domains import RNDomain
 from pymloc.model.dynamical_system.flow_problem import LinearFlow
 from pymloc.model.dynamical_system.initial_value_problem import InitialValueProblem
@@ -17,6 +17,9 @@ from pymloc.model.optimization.nonlinear_leastsquares import NonLinearLeastSquar
 from pymloc.model.optimization.objectives import NonLinearLeastSquares as NLLSObjective
 from pymloc.model.optimization.objectives.objective import Objective
 from pymloc.model.optimization.optimization import NullOptimization
+from pymloc.model.optimization.parameter_optimal_control import ParameterDependentOptimalControl
+from pymloc.model.optimization.parameter_optimal_control import ParameterLQRConstraint
+from pymloc.model.optimization.parameter_optimal_control import ParameterLQRObjective
 from pymloc.model.sensitivities.boundary_dae import BVPSensitivities
 from pymloc.model.variables import InputStateVariables
 from pymloc.model.variables import NullVariables
@@ -28,6 +31,96 @@ from pymloc.model.variables.time_function import Time
 from pymloc.solvers.dynamical_systems.adjoint_sensitivities import AdjointSensitivitiesSolver
 
 np.set_printoptions(precision=4)
+
+
+@pytest.fixture
+def variables2():
+    loc_vars = InputStateVariables(1, 1, time=Time(0., 2.))
+    hl_vars = ParameterContainer(1, domain=RNDomain(1))
+    return hl_vars, loc_vars
+
+
+@pytest.fixture
+def param_control(variables2):
+
+    ll_vars = NullVariables()
+
+    def e(p, t):
+        return np.array([[1.]])
+
+    def a(p, t):
+        return np.array([[-1.]])
+
+    def b(p, t):
+        return np.array([[1.]])
+
+    def c(p, t):
+        return np.array([[1.]])
+
+    def d(p, t):
+        return np.array([[0.]])
+
+    def f(p, t):
+        return np.array([0.])
+
+    return LinearParameterControlSystem(ll_vars, *variables2, e, a, b, c, d, f)
+
+
+@pytest.fixture
+def initial_value():
+    def ivp(p):
+        return np.array([[2.]])
+
+    return ivp
+
+
+@pytest.fixture
+def q():
+    def q(p, t):
+        return jnp.array([[p**2. - 1.]])
+
+    return q
+
+
+@pytest.fixture
+def s():
+    def s(p, t):
+        return np.zeros((1, 1))
+
+    return s
+
+
+@pytest.fixture
+def r():
+    def r(p, t):
+        return np.array([[1.]])
+
+    return r
+
+
+@pytest.fixture
+def m():
+    def m(p):
+        return np.array([[0.]])
+
+    return m
+
+
+@pytest.fixture
+def pdoc_objective(q, s, r, m, variables2):
+    time = Time(0., 2.)
+    return ParameterLQRObjective(*variables2, time, q, s, r, m)
+
+
+@pytest.fixture
+def pdoc_constraint(variables2, param_control, initial_value):
+    return ParameterLQRConstraint(*variables2, param_control, initial_value)
+
+
+@pytest.fixture
+def pdoc_object(variables2, pdoc_objective, pdoc_constraint):
+    return ParameterDependentOptimalControl(*variables2, pdoc_objective,
+                                            pdoc_constraint)
 
 
 #Nonlinear Least Squres fixtures
