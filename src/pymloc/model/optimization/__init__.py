f1e2de41b2ce84ab9d6ab004b1ce96d3cacdd9f8
Daniel Bankmann <bankmann@math.tu-berlin.de>
2020-04-14 12:16:27 +0200

    Add several optimizations to the optimizations namespace.

--- src/pymloc/model/optimization/__init__.py
+++ src/pymloc/model/optimization/__init__.py
@@ -1,2 +1,5 @@
 from . import constraints
 from . import objectives
+from .nonlinear_leastsquares import NonLinearLeastSquares
+from .optimal_control import LQOptimalControl
+from .parameter_optimal_control import ParameterDependentOptimalControl
