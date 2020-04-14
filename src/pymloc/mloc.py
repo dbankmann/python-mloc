9cb979fe32a2a8f56744cc74c577b71e15cdf4c0
Daniel Bankmann <bankmann@math.tu-berlin.de>
2020-04-14 11:28:12 +0200

    Make MultiLevelOptimalControl class inherit from Solvable. Use variables instead of solvers per hierarchy level.

--- src/pymloc/mloc.py
+++ src/pymloc/mloc.py
@@ -1,20 +1,33 @@
 import numpy as np
+
+from .model import Solvable
+
+
 """
 Definition of multilevel optimization problems as in thesis.
 We assume that the constraint function G_i are equal for all i.
 """
 
 
-class MultiLevelOptimalControl(object):
+class MultiLevelOptimalControl(Solvable):
     """Driver routine that initializes and handles all the solvers.
     """
-    def __init__(self, optimizations, solvers):
+    def __init__(self, optimizations, variables):
         self.optimizations = optimizations
         self.levels = len(self.optimizations)
         assert self.levels > 0
-        self.solvers = solvers
-        assert len(solvers) == self.levels
+        self.variables = variables
+        assert len(variables) == self.levels
         if self.levels == 2:
             self.is_bilevel = True
         else:
             self.is_bilevel = False
+        super().__init__()
+
+    @property
+    def lowest_opt(self):
+        return self.optimizations[-1]
+
+    @property
+    def highest_opt(self):
+        return self.optimizations[0]
