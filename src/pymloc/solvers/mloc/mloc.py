23cb1fe6b565ac70c179c950b91c1e380d59c4be
Daniel Bankmann <bankmann@math.tu-berlin.de>
2020-04-14 12:36:49 +0200

    Add MultiLevelIterative Solver for MultiLevelOptimalControl objects.

---
+++ src/pymloc/solvers/mloc/mloc.py
@@ -0,0 +1,43 @@
+from ...mloc import MultiLevelOptimalControl
+from ...solver_container import solver_container_factory
+from .. import BaseSolver
+
+
+class MultiLevelIterativeSolver(BaseSolver):
+    def __init__(self, model):
+        super().__init__(model)
+        self._opts = model.optimizations
+        self._hopt = model.highest_opt
+        self._lopt = model.lowest_opt
+        self._vars = model.variables
+
+    def _run(self, *args, **kwargs):
+        self._check_variables()
+        self._set_associations()
+        upperopt = self._hopt
+        sol = upperopt.solve(*args, **kwargs)
+        return sol
+
+    def _check_variables(self):
+        for variable in self._vars:
+            if variable.current_values is None:
+                raise ValueError(
+                    "All Variables need to be initialized and the current value of {} is None."
+                    .format(variable))
+
+    def _set_associations(self):
+        for variable, optimization in zip(self._vars, self._opts):
+            variable.associated_problem = optimization.get_localized_object()
+            variable.sensitivity_problem = optimization
+
+    def _init_upper_level(self):
+        self._hopt.init_solver()
+
+    def _init_solvers(self):
+        for opt in self._opts:
+            opt.init_solver()
+
+
+solver_container_factory.register_solver(MultiLevelOptimalControl,
+                                         MultiLevelIterativeSolver,
+                                         default=True)
