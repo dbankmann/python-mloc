b30c14f391ee3abf777f01c3ef27a1e289271f6d
Daniel Bankmann <bankmann@math.tu-berlin.de>
2020-04-16 20:27:25 +0200

    Add LevelFilter for logging.

---
+++ src/pymloc/level_filter.py
@@ -0,0 +1,18 @@
+import logging
+import re
+
+from .solvers.base_solver import solver_level
+
+
+class LevelFilter(logging.Filter):
+    def filter(self, record):
+        level = solver_level.level
+
+        indents = level * '\t'
+        record.msg = record.msg.replace('\n', '\n' + indents)
+        record.level_indent = indents
+        return level < 4
+
+
+rootlogger = logging.getLogger()
+rootlogger.handlers[0].addFilter(LevelFilter())
