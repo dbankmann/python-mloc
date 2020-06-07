Fix bug in sensitivity computation for nontrivial selector. Add tests.

src/pymloc/model/optimization/optimal_control.py
@@ -72,19 +72,26 @@ class LQOptimalControl(LocalOptimizationObject):
             fcal_arr[:self._nn] = fcontr(t)
             return fcal_arr
 
+        variables = StateVariablesContainer(dim)
+        self.variables.link_variable(variables, overwrite=True)
+        dyn_sys = LinearFlowRepresentation(variables, ecal, acal, fcal, dim)
+        free_dae = self.constraint.control_system.free_dae
+
         gamma_0 = np.zeros((dim, dim))
         gamma_f = np.zeros((dim, dim))
         gamma_0[:self._nn, self._nn:] = econtr(self._time.t_0)
         gamma_f[self._nn:2 * self._nn, :self._nn] = np.identity(self._nn)
-        gamma_f[self._nn:2 * self._nn,
-                self._nn:2 * self._nn] = self.objective.final_weight
+        gamma_f[self._nn:2 * self._nn, self._nn:2 * self._nn] = free_dae.eplus(
+            self._time.t_f).T @ self.objective.final_weight
         gamma_rhs = np.zeros((dim, ))
-        gamma_rhs[:self._nn] = self.constraint.initial_value
+        gamma_rhs[:self._nn] = free_dae.e(
+            self._time.t_0) @ self.constraint.initial_value
+        rank = free_dae.rank
+        z_gamma = np.zeros((dim, 2 * rank))
+        z_gamma[:self._nn, :rank] = free_dae.z1(self._time.t_f)
+        z_gamma[self._nn:2 * self._nn, rank:] = free_dae.t2(self._time.t_0)
 
-        variables = StateVariablesContainer(dim)
-        self.variables.link_variable(variables, overwrite=True)
-        dyn_sys = LinearFlowRepresentation(variables, ecal, acal, fcal, dim)
-        bvs = BoundaryValues(gamma_0, gamma_f, gamma_rhs)
+        bvs = BoundaryValues(gamma_0, gamma_f, gamma_rhs, z_gamma)
         flow_prob = LinearFlow(self._time, dyn_sys)
         ivp_prob = InitialValueProblem(self.constraint.initial_value,
                                        self._time, dyn_sys)

src/pymloc/solvers/dynamical_systems/adjoint_sensitivities.py
@@ -280,8 +280,8 @@ class AdjointSensitivitiesSolver(SensitivitiesSolver):
             incr_0 = self._sel_times_projector
 
         xi = self._xi_part @ np.block([[
-            e0.T @ adjoint_solution.solution[..., 0] + incr_0
-        ], [ef.T @ adjoint_solution.solution[..., -1] + incr_f]])
+            e0.T @ adjoint_solution.solution[..., 0] + incr_0.T
+        ], [ef.T @ adjoint_solution.solution[..., -1] + incr_f.T]])
 
         return xi
 

tests/test_examples/test_parameter_fitting.py
@@ -48,15 +48,92 @@ def f_nlsq():
 
 
 @pytest.fixture
+def f_nlsq_3():
+    theta = 2.
+    t0 = 0.
+    tf = 2.
+    x01 = 2.
+    time = Time(t0, tf)
+    t2 = 1.3
+    t1 = 1.
+
+    def f(ll_vars, hl_vars, loc_vars):
+        sol1 = compute_ref_sol(theta, time, x01, t1)
+        sol2 = compute_ref_sol(theta, time, x01, t2)
+        sol0 = compute_ref_sol(theta, time, x01, t0)
+        solf = compute_ref_sol(theta, time, x01, tf)
+        f1 = ll_vars(t1)[1] - sol1[1]
+        f2 = ll_vars(t2)[1] - sol2[1]
+        f0 = ll_vars(t0)[1] - sol0[1]
+        ff = ll_vars(tf)[1] - solf[1]
+        return np.hstack((f0, f1, f2, ff))
+
+    return f
+
+
+def compute_ref_sol_augmented(theta, time, x01, t1):
+    ref_sol = compute_ref_sol(theta, time, x01, t1)
+    return np.array([ref_sol[0], 0., ref_sol[1], 0., ref_sol[2]])
+
+
+@pytest.fixture
+def f_nlsq_2():
+    theta = 2.
+    t0 = 0.
+    tf = 2.
+    x01 = 2.
+    time = Time(t0, tf)
+    t2 = 1.3
+    t1 = 1.
+
+    def f(ll_vars, hl_vars, loc_vars):
+        sol1 = compute_ref_sol_augmented(theta, time, x01, t1)
+        sol2 = compute_ref_sol_augmented(theta, time, x01, t2)
+        sol0 = compute_ref_sol_augmented(theta, time, x01, t0)
+        solf = compute_ref_sol_augmented(theta, time, x01, tf)
+        f1 = ll_vars(t1)[2:] - sol1[2:]
+        f2 = ll_vars(t2)[2:] - sol2[2:]
+        f0 = ll_vars(t0)[2:] - sol0[2:]
+        ff = ll_vars(tf)[2:] - solf[2:]
+        return np.hstack((f0, f1, f2, ff))
+
+    return f
+
+
+@pytest.fixture
 def nlsq_obj(f_nlsq, variables2):
     variables = (variables2[1], NullVariables(), variables2[0])
     return optimization.objectives.NonLinearLeastSquares(*variables, f_nlsq)
 
 
 @pytest.fixture
+def nlsq_obj_2(f_nlsq_2, variables3):
+    variables = (variables3[1], NullVariables(), variables3[0])
+    return optimization.objectives.NonLinearLeastSquares(*variables, f_nlsq_2)
+
+
+@pytest.fixture
+def nlsq_obj_3(f_nlsq_3, variables2):
+    variables = (variables2[1], NullVariables(), variables2[0])
+    return optimization.objectives.NonLinearLeastSquares(*variables, f_nlsq_3)
+
+
+@pytest.fixture
 def nlsq(nlsq_obj, variables2):
     variables = (variables2[1], NullVariables(), variables2[0])
     return NonLinearLeastSquares(nlsq_obj, *variables)
+
+
+@pytest.fixture
+def nlsq_3(nlsq_obj_3, variables2):
+    variables = (variables2[1], NullVariables(), variables2[0])
+    return NonLinearLeastSquares(nlsq_obj_3, *variables)
+
+
+@pytest.fixture
+def nlsq_2(nlsq_obj_2, variables3):
+    variables = (variables3[1], NullVariables(), variables3[0])
+    return NonLinearLeastSquares(nlsq_obj_2, *variables)
 
 
 class TestParameterFitting:
@@ -67,19 +144,39 @@ class TestParameterFitting:
         with pytest.raises(ValueError):
             mloc.init_solver()
 
-    def test_mloc_solver(self, pdoc_object, nlsq, variables2):
-        optimizations = [nlsq, pdoc_object]
+    def test_mloc_solver(self, pdoc_object, nlsq_3, variables2):
+        optimizations = [nlsq_3, pdoc_object]
         variables = (variables2[0], variables2[1])
         variables[0].current_values = np.array([1.])
         # TODO: Initialize correctly
         variables[1].current_values = np.array([])
         variables[1].time.grid = np.array([1., 1.3])
+        pdoc_object.ll_sens_selector_shape = (1, 3)
+        pdoc_object.ll_sens_selector = lambda p: np.array([[0., 1., 0.]])
         logger = logging.getLogger("pymloc.solvers.nonlinear.gauss_newton")
         logger.setLevel(logging.DEBUG)
         mloc = MultiLevelOptimalControl(optimizations, variables)
         mloc.init_solver(abs_tol=1e-1, rel_tol=1e-1)
         solution = mloc.solve()
-        assert np.allclose(solution.solution, 2., atol=1e-2)
+        assert np.allclose(solution.solution, 2., atol=1e-1)
+
+    def test_mloc_solver_precise(self, pdoc_object, nlsq_3, variables2):
+        optimizations = [nlsq_3, pdoc_object]
+        variables = (variables2[0], variables2[1])
+        variables[0].current_values = np.array([1.])
+        # TODO: Initialize correctly
+        variables[1].current_values = np.array([])
+        variables[1].time.grid = np.array([1., 1.3])
+        pdoc_object.ll_sens_selector_shape = (1, 3)
+        pdoc_object.ll_sens_selector = lambda p: np.array([[0., 1., 0.]])
+        logger = logging.getLogger("pymloc.solvers.nonlinear.gauss_newton")
+        logger.setLevel(logging.DEBUG)
+        mloc = MultiLevelOptimalControl(optimizations, variables)
+
+        np.set_printoptions(precision=8)
+        mloc.init_solver(abs_tol=1e-3, rel_tol=1e-3)
+        solution = mloc.solve()
+        assert np.allclose(solution.solution, 2., atol=9e-2)
 
     def test_mloc_solver_at_final(self, pdoc_object, nlsq, variables2):
         optimizations = [nlsq, pdoc_object]
@@ -95,3 +192,24 @@ class TestParameterFitting:
         variables[0].associated_problem._solver_instance.upper_eta = 1.
         solution = mloc.solve()
         assert np.allclose(solution.solution, 2., atol=1e-12)
+
+    @pytest.mark.xfail
+    def test_mloc_solver_2(self, pdoc_object_2, nlsq_2, variables3):
+        optimizations = [nlsq_2, pdoc_object_2]
+        variables = (variables3[0], variables3[1])
+        variables[0].current_values = np.array([1., 2.])
+        # TODO: Initialize correctly
+        variables[1].current_values = np.array([])
+        variables[1].time.grid = np.array([1., 1.3])
+        logger = logging.getLogger("pymloc.solvers.nonlinear.gauss_newton")
+        logger.setLevel(logging.DEBUG)
+        pdoc_object_2.ll_sens_selector_shape = (3, 5)
+        pdoc_object_2.ll_sens_selector = lambda p: np.block(
+            [[np.zeros((2, 3))], [np.eye(3)]]).T
+        # nlsq_2.ll_sens_selector_shape = (5, 3)
+        # nlsq_2.ll_sens_selector = lambda p: np.linalg.block(
+        #     [[np.zeros((2, 3))], [np.eye(3)]])
+        mloc = MultiLevelOptimalControl(optimizations, variables)
+        mloc.init_solver(abs_tol=1e-1, rel_tol=1e-1)
+        solution = mloc.solve()
+        assert np.allclose(solution.solution, 2., atol=1e-2)